6ccaaa1c4412f3d5f86e97e9c7e33df1109082e8
Michael Mosson <Michael_Mosson@ymail.com>
2016-11-12 00:09:15 +0000

    v0.3c
    Added a terminal in the game level (located next to the elevator door).
    
    Added new script - Desk.cs (this script has to be renamed) the script
    controls the mouse events for the in game terminal.
    
    Added new level Reduction 6
    
    Added chairs

---
+++ Assets/Scripts/Desk.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Desk : MonoBehaviour {
+
+	public bool isOpen;
+
+	public Text alertText;
+
+	public enum State{
+		Open,
+		Closed,
+		Inbetween
+	}
+
+	public State state;
+
+	// Use this for initialization
+	void Start () {
+		state = Desk.State.Closed;
+		isOpen = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnMouseEnter(){
+
+		alertText.text = "Press RMB to load HAX.EXE";
+		Debug.Log ("Enter");
+	}
+
+	public void OnMouseExit () {
+		alertText.text = "";
+		Debug.Log ("Exit");
+	}
+
+	public void OnMouseUp (){
+		Debug.Log ("Up");
+	}
+
+	private void Open (){
+		Debug.Log ("Open");
+	}
+
+	private void Close (){
+		Debug.Log ("Close");
+	}
+}
