04c275d41ddb4d14bbb5222ee46b937ca01271ec
Michael Mosson <Michael_Mosson@ymail.com>
2016-11-29 15:30:33 +0000

    V0.5a
    -Added some assets to make the level feel alive
    -Retextured the Sun, temporary texture is used - animated one should be
    used
    -Retextured the floor with a more Sci-Fi texture
    -Added a window so player can see the star system we have created
    -Started work on notes system, more coming soon
    -Worked on finishing terminal gameobject
    -Changed dev platform from Mac to Windows, also using a newer version of
    Unity
    -Removed assets  from scene that were not needed, or were clutter
    -Minor fixes and changes

---
+++ Assets/Scripts/noteController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class noteController : MonoBehaviour {
+
+    public Text noteHeaderText;
+    public Text noteContentsText;
+    public Text alertText;
+    public GameObject notePanel;
+
+    private UIController uiController;
+
+    public int noteID;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        uiController = GetComponent<UIController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Enter");
+        alertText.text = "Click to read";
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("Exit");
+        alertText.text = "";
+    }
+
+    public void OnMouseUp()
+    {
+        OpenNote();
+    }
+
+    public void OpenNote()
+    {
+        notePanel.SetActive(true);
+        setTextsOnActive();
+    }
+
+    public void setTextsOnActive()
+    {
+    
+    }
+}
